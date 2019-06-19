e26e8ced6f010fa6f8c2ae685d93cd901cfc253c
Israel <213withani@gmail.com>
2019-06-18 21:47:12 -0700

    Updated company position title to a complimentary color.

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -1,15 +1,17 @@
 import React, { Fragment } from 'react';
-import { PRIMARY_COLOR } from '../styles';
+import { PRIMARY_COLOR, H1_SIZE } from '../styles';
 
 const Education = ( props ) => {
     const { header, description } = props.data.educationData;
-
+    const data = description.split('-');
+    const companyName = data[0];
+    const position = data[1] || '';
     return (
       <Fragment>
           <h1 style={ styles.header }>{ header }</h1>
-          <h2 style={styles.description}>
-            { description }
-          </h2>
+          <span style={ styles.title }>{companyName}</span>
+          {position && <span>-</span>}
+          <span style={ styles.position }>{position}</span>
       </Fragment>
     );
 
@@ -17,7 +19,15 @@ const Education = ( props ) => {
 
 const styles = {
   header: { textTransform: 'uppercase' },
-  description: { color: PRIMARY_COLOR },
+  title: {
+    ...H1_SIZE,
+  },
+  position: {
+      fontSize: 25,
+      fontWeight: 500,
+      marginBottom: 0,
+      color: 'orange',
+  },
 };
 
 export default Education;
